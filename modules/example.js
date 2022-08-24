536b13241c14ad1adbcf4a9fb726120ed3bddeca
Ghex <69035887+GhexterCortes@users.noreply.github.com>
2022-08-24 13:44:13 +0000

    update

--- modules/example.js
+++ modules/example.js
@@ -2,7 +2,7 @@ const { MessageCommandBuilder, SlashCommandBuilder } = require('reciple');
 
 class Example {
   constructor() {
-    this.versions = '^5.0.0';
+    this.versions = '^5.3.0';
     this.commands = [
       new MessageCommandBuilder()
         .setName('ping')
