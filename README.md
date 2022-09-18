9679b6a7fd32befc1c16dab255e89e4aab9df65a
Ghex <ghextershumies@gmail.com>
2022-09-18 11:16:52 +0000

    Update and remove axios

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Reciple bot template
 
-Run `npm start` to start your bot.
+A [discord.js](https://npmjs.org/package/discord.js) bot based on [reciple](https://npmjs.org/package/reciple) handler.
+
+* Run `npm start` or `npx reciple` to start your bot.
+
+[Reciple GitHub](https://github.com/FalloutStudios/Reciple) | [Discord.js GitHub](https://github.com/discordjs/discord.js)
--- modules/example.js
+++ modules/example.js
@@ -1,36 +1,41 @@
-const { MessageCommandBuilder, SlashCommandBuilder, CommandBuilderType } = require('reciple');
+const { CommandBuilderType, CommandHaltReason } = require('reciple');
 
-class Example {
-  constructor() {
-    this.versions = '^5.4.0';
-    this.commands = [
-      {
-        type: CommandBuilderType.MessageCommand,
-        name: 'ping',
-        description: 'Pong!',
-        options: [],
-        cooldown: 1000 * 10,
-        excute: e => e.message.reply("Pong!")
-      },
-      {
-        type: CommandBuilderType.SlashCommand,
-        name: 'ping',
-        description: 'Pong!',
-        cooldown: 1000 * 10,
-        execute: e => e.interaction.reply("Pong!")
+module.exports = {
+  versions: '^5.4.2',
+  commands: [
+    {
+      type: CommandBuilderType.MessageCommand,
+      name: 'ping',
+      description: 'Pong!',
+      cooldown: 1000 * 10,
+      execute: e => e.message.reply("Pong!"),
+      halt: e => {
+        if(e.reason == CommandHaltReason.Error) {
+          e.executeData.message.reply(String(e.error));
+          return true;
+        }
       }
-    ];
-  }
-
+    },
+    {
+      type: CommandBuilderType.SlashCommand,
+      name: 'ping',
+      description: 'Pong!',
+      cooldown: 1000 * 10,
+      execute: e => e.interaction.reply("Pong!"),
+      halt: e => {
+        if(e.reason == CommandHaltReason.Error) {
+           e.executeData.interaction.reply(String(e.error));
+          return true;
+        }
+      }
+    }
+  ],
   onStart(client) {
     client.logger.log("Module started");
 
     return true;
-  }
-
+  },
   onLoad(client) {
     client.logger.log("Module loaded");
   }
-}
-
-module.exports = new Example();
+};
